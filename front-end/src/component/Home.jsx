5eb3fbf7220791e5e29e1ca4173c81e8436370e4
ManueleSeretti <136794705+ManueleSeretti@users.noreply.github.com>
2023-12-19 08:52:56 +0100

    layout home page

--- front-end/src/component/Home.jsx
+++ front-end/src/component/Home.jsx
@@ -5,46 +5,61 @@ const { Container, Row, Col, Card } = require("react-bootstrap");
 
 const Home = () => {
   return (
-    <Container fluid style={{ backgroundImage: `url(${backGround})`, width: "100%" }}>
-      <TopBar />
-      <Row className="justify-content-between">
-        <Col xs="4">
-          {" "}
-          <Card className="mx-auto " style={{ width: "18rem", backgroundColor: "rgba(255, 255, 255, 0.5)" }}>
-            <Card.Img variant="top" src="holder.js/100px180" />
-            <Card.Body>
-              <Card.Title>Card Title</Card.Title>
-              <Card.Text>
-                Some quick example text to build on the card title and make up the bulk of the card's content.
-              </Card.Text>
-            </Card.Body>
+    <Container fluid className="p-0">
+      <div
+        style={{
+          backgroundImage: `url(${backGround})`,
+          width: "100%",
+          height: "100vh",
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+        }}
+      >
+        <TopBar />
+        <div className="d-flex flex-column justify-content-between align-items-between" style={{ height: "80%" }}>
+          <Row className="justify-content-between">
+            <Col xs="4" className="p-5">
+              {" "}
+              <Card className="mx-auto  " style={{ backgroundColor: "rgba(255, 255, 255, 0.5)" }}>
+                <Card.Img variant="top" src="holder.js/100px180" />
+                <Card.Body>
+                  <Card.Title>Card Title</Card.Title>
+                  <Card.Text>
+                    Some quick example text to build on the card title and make up the bulk of the card's content.
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col xs="4" className="p-5">
+              {" "}
+              <Card className="mx-auto " style={{ backgroundColor: "rgba(255, 255, 255, 0.5)" }}>
+                <Card.Img variant="top" src="holder.js/100px180" />
+                <Card.Body>
+                  <Card.Title>Card Title</Card.Title>
+                  <Card.Text>
+                    Some quick example text to build on the card title and make up the bulk of the card's content.
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col xs="4" className="p-5">
+              {" "}
+              <Card className="mx-auto " style={{ backgroundColor: "rgba(255, 255, 255, 0.5)" }}>
+                <Card.Img variant="top" src="holder.js/100px180" />
+                <Card.Body>
+                  <Card.Title>Card Title</Card.Title>
+                  <Card.Text>
+                    Some quick example text to build on the card title and make up the bulk of the card's content.
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+          </Row>
+          <Card body className="bg-dark text-warning">
+            Vedi subito le vetture disponibili
           </Card>
-        </Col>
-        <Col xs="4">
-          {" "}
-          <Card className="mx-auto" style={{ width: "18rem", backgroundColor: "rgba(255, 255, 255, 0.5)" }}>
-            <Card.Img variant="top" src="holder.js/100px180" />
-            <Card.Body>
-              <Card.Title>Card Title</Card.Title>
-              <Card.Text>
-                Some quick example text to build on the card title and make up the bulk of the card's content.
-              </Card.Text>
-            </Card.Body>
-          </Card>
-        </Col>
-        <Col xs="4">
-          {" "}
-          <Card className="mx-auto" style={{ width: "18rem", backgroundColor: "rgba(255, 255, 255, 0.5)" }}>
-            <Card.Img variant="top" src="holder.js/100px180" />
-            <Card.Body>
-              <Card.Title>Card Title</Card.Title>
-              <Card.Text>
-                Some quick example text to build on the card title and make up the bulk of the card's content.
-              </Card.Text>
-            </Card.Body>
-          </Card>
-        </Col>
-      </Row>
+        </div>
+      </div>
     </Container>
   );
 };
